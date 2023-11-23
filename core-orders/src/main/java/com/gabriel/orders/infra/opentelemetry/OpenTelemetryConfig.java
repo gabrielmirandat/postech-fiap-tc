3886bcbc4ca397c2461fbc73ba27cdc290eca401
Gabriel Miranda <gabrielmirandatt@gmail.com>
2023-11-23 18:33:10 -0300

    feat: updates in running with docker, new relic and open telemetry

--- core-orders/src/main/java/com/gabriel/orders/infra/opentelemetry/OpenTelemetryConfig.java
+++ core-orders/src/main/java/com/gabriel/orders/infra/opentelemetry/OpenTelemetryConfig.java
@@ -5,17 +5,28 @@ import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenTelemetryConfig {
 
+    @Value("${newrelic.otlp.url}")
+    private String newRelicOtlpUrl;
+
+    @Value("${newrelic.app.name}")
+    private String newRelicAppName;
+
+    @Value("$newrelic.service.key")
+    private String newRelicServiceKey;
+
     @Bean
     public Tracer tracer() {
         // Configure the exporter
         OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
-            .setEndpoint("http://localhost:4317") // Replace with New Relic trace API endpoint
+            .setEndpoint(newRelicOtlpUrl) // Use the appropriate endpoint
+            .addHeader("api-key", newRelicServiceKey)
             .build();
 
         // Configure the SDK with the exporter
@@ -26,6 +37,6 @@ public class OpenTelemetryConfig {
                     .build()
             ).build();
 
-        return openTelemetrySdk.getTracer("core-orders-app"); // Replace with your application's name
+        return openTelemetrySdk.getTracer(newRelicAppName); // Replace with your application's name
     }
 }
