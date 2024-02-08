adfdd0c44d97317588b16934d2ebcb6dad37b6d7
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-02-08 00:51:14 -0300

    feat: add initial config for k8s

---
+++ .k8s/start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Define the directory containing your Kubernetes YAML files
+K8S_RESOURCE_DIR="."
+
+# Start Minikube
+# minikube start --driver=docker
+# minikube status
+# kubectl config current-context
+
+# Apply the configurations
+echo "Starting all Kubernetes resources..."
+kubectl apply -f $K8S_RESOURCE_DIR/mongodb-secrets.yaml
+kubectl apply -f $K8S_RESOURCE_DIR/mongodb-deployment.yaml
+kubectl apply -f $K8S_RESOURCE_DIR/mongodb-service.yaml
+kubectl apply -f $K8S_RESOURCE_DIR/redis-deployment.yaml
+kubectl apply -f $K8S_RESOURCE_DIR/redis-service.yaml
+kubectl apply -f $K8S_RESOURCE_DIR/zookeeper-deployment.yaml
+kubectl apply -f $K8S_RESOURCE_DIR/zookeeper-service.yaml
+kubectl apply -f $K8S_RESOURCE_DIR/kafka-deployment.yaml
+kubectl apply -f $K8S_RESOURCE_DIR/kafka-service.yaml
+#kubectl apply -f $K8S_RESOURCE_DIR/menu-api-deployment.yaml
+#kubectl apply -f $K8S_RESOURCE_DIR/menu-api-service.yaml
+#kubectl apply -f $K8S_RESOURCE_DIR/orders-api-deployment.yaml
+#kubectl apply -f $K8S_RESOURCE_DIR/orders-api-service.yaml
+# Add any additional resources here
+
+echo "All resources have been started."
+
+# List all resources
+kubectl get all
+
+# Open the Kubernetes dashboard
+minikube dashboard
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-docker exec -it postech_kafka kafka-topics --create --topic menub --bootstrap-server localhost:9092
-
-#docker exec -it postech_kafka kafka-topics --describe --topic menu --bootstrap-server localhost:9092
-
-echo '{"id":"menuProductAdded","event":{"productID":"123","name":"Pizza","value":"10.99"}}' | docker exec \
--i postech_kafka kafka-console-producer --bootstrap-server localhost:9092 --topic menu
