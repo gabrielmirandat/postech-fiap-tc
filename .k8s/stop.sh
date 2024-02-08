c37fbbadcef4a245146741dd1e728dcab70f1173
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-02-08 00:51:14 -0300

    feat: add initial config for k8s

---
+++ .k8s/stop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the directory containing your Kubernetes YAML files
+K8S_RESOURCE_DIR="."
+
+# Delete the configurations for resources in reverse order of dependency
+echo "Stopping all Kubernetes resources..."
+#kubectl delete -f $K8S_RESOURCE_DIR/orders-service-service.yaml
+#kubectl delete -f $K8S_RESOURCE_DIR/orders-service-deployment.yaml
+#kubectl delete -f $K8S_RESOURCE_DIR/menu-service-service.yaml
+#kubectl delete -f $K8S_RESOURCE_DIR/menu-service-deployment.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/kafka-service.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/kafka-deployment.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/zookeeper-service.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/zookeeper-deployment.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/redis-service.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/redis-deployment.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/mongodb-service.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/mongodb-deployment.yaml
+kubectl delete -f $K8S_RESOURCE_DIR/mongodb-secrets.yaml
+# Add any additional resources here in reverse order they were started
+
+echo "All resources have been stopped and deleted."
+
+# Stop Minikube
+minikube stop
