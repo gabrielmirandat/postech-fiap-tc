e5caf2ea04d9a1e68cc1914472419b385ba7cc08
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-02-25 19:17:37 -0300

    fixes: orders contract test

--- orders/src/main/java/com/gabriel/orders/adapter/driver/api/OrdersHttpExceptionHandler.java
+++ orders/src/main/java/com/gabriel/orders/adapter/driver/api/OrdersHttpExceptionHandler.java
@@ -11,6 +11,7 @@ import com.gabriel.specs.orders.models.ErrorResponse;
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -25,7 +26,8 @@ public class OrdersHttpExceptionHandler {
     }
 
     @ExceptionHandler({MethodArgumentTypeMismatchException.class,
-        ConversionFailedException.class})
+        ConversionFailedException.class,
+        HttpMessageNotReadableException.class})
     public ResponseEntity<ErrorResponse> handleConversionFailed(Exception exception) {
         return convertHttpAndSend(BadRequest.from(exception));
     }
