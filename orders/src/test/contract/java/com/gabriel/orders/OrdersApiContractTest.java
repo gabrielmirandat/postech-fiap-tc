be336d209a330151a0b19f9185514840b847b362
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-02-25 21:01:19 -0300

    fixes: orders contract test

--- orders/src/test/contract/java/com/gabriel/orders/OrdersApiContractTest.java
+++ orders/src/test/contract/java/com/gabriel/orders/OrdersApiContractTest.java
@@ -8,61 +8,70 @@ import com.gabriel.orders.adapter.container.KafkaTestContainer;
 import com.gabriel.orders.adapter.container.MongoDBTestContainer;
 import com.gabriel.orders.adapter.container.RedisTestContainer;
 import com.gabriel.orders.adapter.driven.api.MenuGrpcClient;
+import com.gabriel.orders.adapter.driver.api.mapper.StringToOrderStatusDTOConverter;
 import com.gabriel.orders.core.OrderMock;
+import com.gabriel.orders.core.domain.model.Extra;
 import com.gabriel.orders.core.domain.model.Order;
-import com.gabriel.orders.core.domain.port.MenuRepository;
-import com.gabriel.orders.core.domain.port.MenuSubscriber;
-import com.gabriel.orders.core.domain.port.OrderPublisher;
-import com.gabriel.orders.core.domain.port.OrderRepository;
+import com.gabriel.orders.core.domain.model.OrderStatus;
+import com.gabriel.orders.core.domain.model.Product;
+import com.gabriel.orders.core.domain.port.*;
 import com.gabriel.orders.infra.grpc.GrpcClientConfig;
 import com.gabriel.orders.infra.kafka.KafkaConfig;
 import com.gabriel.orders.infra.mongodb.MongoDbConfig;
 import com.gabriel.orders.infra.redis.RedisConfig;
 import com.gabriel.orders.infra.serializer.SerializerConfig;
+import com.gabriel.orders.infra.web.WebConfig;
 import in.specmatic.test.SpecmaticJUnitSupport;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
 import java.io.File;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT) // This will start the server on a random port
-@Import({MongoDbConfig.class, GrpcClientConfig.class, RedisConfig.class, KafkaConfig.class, SerializerConfig.class})
+@Import({MongoDbConfig.class, GrpcClientConfig.class, RedisConfig.class,
+    KafkaConfig.class, SerializerConfig.class,
+    StringToOrderStatusDTOConverter.class, WebConfig.class})
 @ContextConfiguration(classes =
-    {OrdersApplication.class, MongoDBTestContainer.class, GrpcServerTestContainer.class, RedisTestContainer.class, KafkaTestContainer.class}
+    {OrdersApplication.class, MongoDBTestContainer.class, GrpcServerTestContainer.class,
+        RedisTestContainer.class, KafkaTestContainer.class}
 )
+@ActiveProfiles("test")
 public class OrdersApiContractTest extends SpecmaticJUnitSupport {
+
+    @MockBean
+    private MenuRepository menuRepository;
+    @MockBean
+    private OrderRepository orderRepository;
+    @MockBean
+    private MenuGrpcClient menuGrpcClient;
+    @MockBean
+    private OrderPublisher orderPublisher;
+    @MockBean
+    private MenuSubscriber menuSubscriber;
 
     @LocalServerPort
     private int port; // Injects the port the server is running on
 
-    @MockBean
-    private MenuGrpcClient menuGrpcClient;
-
-    @MockBean
-    private OrderPublisher orderPublisher;
-
-    @MockBean
-    private MenuRepository menuRepository;
-
-    @MockBean
-    private OrderRepository orderRepository;
-
-    @MockBean
-    private MenuSubscriber menuSubscriber;
-
     private Order order;
+    private Order orderFull;
+    private Product product;
+    private Extra extra;
 
     @BeforeAll
     public static void setup() {
@@ -87,20 +96,31 @@ public class OrdersApiContractTest extends SpecmaticJUnitSupport {
     @BeforeEach
     public void initialize() throws JsonProcessingException {
         order = OrderMock.generateBasic();
+        orderFull = OrderMock.generateFull();
+        product = OrderMock.generateProduct();
+        extra = OrderMock.generateExtra();
+
         when(orderRepository.getByTicket(eq("11111111")))
-            .thenReturn(OrderMock.generateBasic());
+            .thenReturn(order);
+        when(orderRepository.searchBy(eq(new OrderSearchParameters(OrderStatus.CREATED))))
+            .thenReturn(List.of(orderFull));
         when(menuRepository.getProduct(eq(new ProductID("11111111-PRDC-1111-11-11"))))
-            .thenReturn(OrderMock.generateProduct());
+            .thenReturn(product);
         when(menuRepository.existsProduct(eq(new ProductID("11111111-PRDC-1111-11-11"))))
             .thenReturn(true);
         when(menuRepository.existsProduct(eq(new ProductID("11111111-PRDC-1111-11-19"))))
             .thenThrow(RuntimeException.class);
         when(menuRepository.getExtra(eq(new IngredientID("11111111-INGR-1111-11-11"))))
-            .thenReturn(OrderMock.generateExtra());
+            .thenReturn(extra);
         when(menuRepository.existsExtra(eq(new IngredientID("11111111-INGR-1111-11-11"))))
             .thenReturn(true);
 
         System.setProperty("host", "localhost");
         System.setProperty("port", String.valueOf(port));
+    }
+
+    @AfterEach
+    public void cleanup() {
+        Mockito.reset(); // Correctly clears all mocks
     }
 }
