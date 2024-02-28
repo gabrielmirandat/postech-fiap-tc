a2af19f70852001eb180c8903336ac753f1ff9b2
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-02-27 23:32:35 -0300

    fix: updating tests structure

---
+++ orders/src/test/java/unit/com/gabriel/orders/core/application/usecase/RetrieveOrderUseCaseTest.java
@@ -0,0 +1,48 @@
+package unit.com.gabriel.orders.core.application.usecase;
+
+import com.gabriel.orders.core.application.query.GetByTicketOrderQuery;
+import com.gabriel.orders.core.application.usecase.RetrieveOrderUseCase;
+import com.gabriel.orders.core.domain.model.Order;
+import com.gabriel.orders.core.domain.model.TicketId;
+import com.gabriel.orders.core.domain.port.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import unit.com.gabriel.orders.core.OrderMock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RetrieveOrderUseCaseTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private RetrieveOrderUseCase retrieveOrderUseCase;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetByTicketId() {
+        // Given
+        TicketId ticketId = new TicketId("12345678");
+        GetByTicketOrderQuery query = new GetByTicketOrderQuery(ticketId);
+        Order expectedOrder = OrderMock.generateBasic();
+        // If your Order class requires specific setup, do it here
+        when(orderRepository.getByTicket(String.valueOf(ticketId))).thenReturn(expectedOrder);
+
+        // When
+        Order actualOrder = retrieveOrderUseCase.getByTicketId(query);
+
+        // Then
+        verify(orderRepository).getByTicket(String.valueOf(ticketId));
+        assertEquals(expectedOrder, actualOrder, "The retrieved order should match the expected one.");
+    }
+}
