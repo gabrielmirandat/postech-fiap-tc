4549583bd48887dce58c282f6267365f872b3a1d
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-03-19 20:26:18 -0300

    Update main.tf

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -12,6 +12,13 @@ terraform {
       version = "3.4.3"
     }
   }
+  cloud {
+    organization = "gabsrell"
+
+    workspaces {
+      name = "learn-terraform-github-actions"
+    }
+  }
   required_version = ">= 1.1.0"
 }
 
