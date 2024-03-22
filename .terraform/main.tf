2dc4b99b088c803473296101276a40b00986d20f
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-03-22 09:10:41 -0300

    fmt terraform

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -57,7 +57,7 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.29"
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
   cluster_addons = {
@@ -67,8 +67,8 @@ module "eks" {
     }
   }
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
