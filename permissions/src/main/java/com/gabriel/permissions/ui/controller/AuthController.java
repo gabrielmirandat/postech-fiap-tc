f5be00b52e27e299c578b829411d97553f87e8cc
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-04-14 02:26:49 -0300

    feat: working spring security

--- permissions/src/main/java/com/gabriel/permissions/ui/controller/AuthController.java
+++ permissions/src/main/java/com/gabriel/permissions/ui/controller/AuthController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-    @Value("${auth0.domain}")
-    private String domain;
+    @Value("${auth0.issuer}")
+    private String issuer;
 
     @Value("${auth0.clientId}")
     private String clientId;
@@ -25,17 +25,20 @@ public class AuthController {
     @Value("${auth0.audience}")
     private String audience;
 
+    @Value("${auth0.scope}")
+    private String scope;
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody CredentialsRequest credentialsRequest) {
-        HttpResponse<String> response = Unirest.post("https://" + domain + "/oauth/token")
+        HttpResponse<String> response = Unirest.post("https://" + issuer + "/oauth/token")
             .header("Content-Type", "application/x-www-form-urlencoded")
             .field("grant_type", "password")
             .field("client_id", clientId)
             .field("client_secret", clientSecret)
+            .field("audience", audience)
+            .field("scope", scope)
             .field("username", credentialsRequest.username())
             .field("password", credentialsRequest.password())
-            .field("audience", audience)
-            .field("scope", "openid profile email")
             .asString();
 
         return ResponseEntity.ok(response.getBody());
