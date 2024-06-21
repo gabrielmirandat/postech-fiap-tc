57692d5f628cb19dab08fa2a8c0242bd80d73533
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-06-21 01:51:12 -0300

    feat: add behavior step and fix tests

--- orders/src/test/java/contract/com/gabriel/orders/adapter/driver/api/OrdersApiContractTest.java
+++ orders/src/test/java/contract/com/gabriel/orders/adapter/driver/api/OrdersApiContractTest.java
@@ -11,6 +11,11 @@ import com.gabriel.orders.core.domain.model.Order;
 import com.gabriel.orders.core.domain.model.OrderStatus;
 import com.gabriel.orders.core.domain.model.Product;
 import com.gabriel.orders.core.domain.port.*;
+import com.gabriel.orders.infra.grpc.MenuGrpcClientConfig;
+import com.gabriel.orders.infra.kafka.KafkaConfig;
+import com.gabriel.orders.infra.mongodb.MongoDbConfig;
+import com.gabriel.orders.infra.redis.RedisConfig;
+import com.gabriel.orders.infra.serializer.SerializerConfig;
 import in.specmatic.test.SpecmaticJUnitSupport;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -20,15 +25,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import utils.com.gabriel.orders.adapter.container.GrpcServerTestContainer;
+import utils.com.gabriel.orders.adapter.container.KafkaTestContainer;
+import utils.com.gabriel.orders.adapter.container.MongoDBTestContainer;
+import utils.com.gabriel.orders.adapter.container.RedisTestContainer;
 import utils.com.gabriel.orders.core.domain.ExtraMock;
 import utils.com.gabriel.orders.core.domain.OrderMock;
 import utils.com.gabriel.orders.core.domain.ProductMock;
-import utils.com.gabriel.orders.infra.SpringContextConfiguration;
+import utils.com.gabriel.orders.infra.SecurityConfig;
 
 import java.io.File;
 import java.util.List;
@@ -36,8 +47,13 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = {OrdersApplication.class, SpringContextConfiguration.class},
-    webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT) // This will start the server on a random port
+@Import({MongoDbConfig.class, MenuGrpcClientConfig.class, RedisConfig.class,
+    KafkaConfig.class, SerializerConfig.class, SecurityConfig.class})
+@ContextConfiguration(classes =
+    {OrdersApplication.class, MongoDBTestContainer.class, GrpcServerTestContainer.class,
+        RedisTestContainer.class, KafkaTestContainer.class}
+)
 @ActiveProfiles("test")
 public class OrdersApiContractTest extends SpecmaticJUnitSupport {
 
@@ -70,7 +86,10 @@ public class OrdersApiContractTest extends SpecmaticJUnitSupport {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
-        SpringContextConfiguration.setProperties(registry);
+        MongoDBTestContainer.mongoProperties(registry);
+        GrpcServerTestContainer.grpcProperties(registry);
+        RedisTestContainer.redisProperties(registry);
+        KafkaTestContainer.kafkaProperties(registry);
     }
 
     @BeforeEach
