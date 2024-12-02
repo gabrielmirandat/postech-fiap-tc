341438c5ad41a2e4c838f593c2765df96068bce1
Gabriel Miranda <gabrielmirandatt@gmail.com>
2024-12-02 00:23:38 -0300

    orders: try behavior bazel tests

--- orders/src/test/behavior/java/com/gabriel/orders/core/application/RunCucumberTest.java
+++ orders/src/test/behavior/java/com/gabriel/orders/core/application/RunCucumberTest.java
@@ -7,11 +7,13 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("behavior.com.gabriel.orders.core.application")
+@SelectPackages("com.gabriel.orders.core.application")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "behavior.com.gabriel.orders.core.application")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.gabriel.orders.core.application")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "classpath:resources/features")
 public class RunCucumberTest {
 }
