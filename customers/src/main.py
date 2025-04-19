fb876676c0dcad8fe85e68b3fc27251356c8d752
Gabriel Miranda <gabrielmirandatt@gmail.com>
2025-04-18 21:50:29 -0300

    feat: customer improvements

--- customers/src/main.py
+++ customers/src/main.py
@@ -21,4 +21,4 @@ async def shutdown():
     await kafka_producer.stop()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8003)
